b858a81414dcf303052982a2dc4e058c0f5ddcc4
HeilskovPJ31 <heilskovpj31@uww.edu>
2015-02-10 14:43:21 -0600

    1.0

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -2,17 +2,29 @@
 
 using namespace std;
 
-int value = 10;
+int value = 3;
 int x;
+
+
 int main(){
 
 cout << "guess a number between 1 and 10: ";
 cin >>x;
 
-if (x == value)
-	cout<<"\n\nCORRECT!!"<<endl;
-else
-	cout<<"\n\nWRONG!"<<endl;
+while(x != value){
+
+cout<<"\n\nGuess again: ";
+cin >>x;
+
+
+	if (x == value){
+		cout<<"\n\nCORRECT!!"<<endl;
+	}
+	else{
+		cout<<"\n\nWRONG!";
+	}
+}
+
 
 
 }
